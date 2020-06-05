b80ccd88e4f54cb855ba0512ef969bc25b1322ce
jared <jared@propdata.net>
2020-06-05 10:03:56 +0200

    Made changes

--- meeting/urls.py
+++ meeting/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib import admin
 
 from . import views
 
@@ -9,3 +10,7 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
+
+admin.site.site_header = "Meetings App"
+admin.site.site_title = "Meetings App"
+admin.site.index_title = "Meetings App"
